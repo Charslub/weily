da896b7c014b0aae0b3c38c39cd7718dd75df4b9
Charslub <1132552771@qq.com>
2020-07-06 23:48:55 +0800

    second commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,27 @@
 <template>
 	<div v-show="isShow" class="login-panel">
-
 		<h3>Login</h3>
 		<div id="login-input-username">
-			<sapn>用户名:</sapn>
+			<span>用户名:</span>
 			<el-input v-model="username"
-			              prefix-icon="el-icon-user"
-			              placeholder="请输入内容"
-			              class="login-input"></el-input>
-		</div><br>
+			          prefix-icon="el-icon-user"
+			          placeholder="请输入用户名"
+			          class="login-input"></el-input>
+		</div>
 		<div id="login-input-password">
 			<span>密 码:</span>
 			<el-input placeholder="请输入密码"
-			             prefix-icon="el-icon-lock"
-			             v-model="password"
-			             show-password
-			             class="login-input"></el-input>
-		</div><br>
+			          prefix-icon="el-icon-lock"
+			          v-model="password"
+			          show-password
+			          class="login-input"></el-input>
+		</div>
 		<div id="login-input-check">
 			<span>验证码:</span>
       <Check @login="check"
-                 ref="check"
-                 class="login-input"></Check>
-		</div><br>
+             ref="check"
+             class="login-input"></Check>
+		</div>
 		<div id="login-input-submit">
 			<el-button type="primary"
 			           @click="login"
@@ -76,20 +75,27 @@
 
 	.login-panel {
 		width: 330px;
-		height: 350px;
-		margin: 0 auto;
+		height: 330px;
+		transition: al1 .2s linear;
 		padding: 10px;
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate( -50%, -50%);
 		border-radius: 10px;
 		background-color: white;
-		box-shadow: 0px 0px 4px rgba(100, 100, 100, .3);
+		box-shadow: 0 0 4px rgba(100, 100, 100, .3);
 	}
 
 	.login-panel div {
 		display: flex;
 		text-align: left;
-		width: 300px;
+		width: 90%;
+		height: 40px;
 		line-height: 40px;
-		margin: 0px auto;
+		margin: 0 auto;
+		margin-top: 10px;
+		margin-bottom: 10px;
 	}
 
 	.login-input {
@@ -97,12 +103,37 @@
 	}
 
 	.login-panel div span {
+		height: 50px;
+		line-height: 50px;
 		flex: 1;
 	}
 
 	#login-input-submit {
-		width: 200px;
+		width: 70%;
+		padding-top: 20px;
 		text-align: center;
+	}
+
+	@media not screen and (min-width: 45em) {
+		.login-panel{
+			width: 95%;
+			min-width: 250px;
+			padding: 0;
+			margin-top: 10px;
+		}
+
+		.login-panel div span {
+			flex: 0;
+			overflow: hidden;
+		}
+
+		.login-panel {
+			border-radius: 0;
+		}
+	}
+
+	h3 {
+		margin-bottom: 10px;
 	}
 
 </style>
